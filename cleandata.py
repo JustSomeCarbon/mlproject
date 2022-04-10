6e78245339cb31297a0c50887039b53a8f42cc46
one_autumnleaf <noe.garcia@student.nmt.edu>
2022-04-10 13:41:41 -0600

    feat: word stemming

--- cleandata.py
+++ cleandata.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
 
 # download stopwords
 nltk.download("stopwords")
@@ -14,16 +15,17 @@ nltk.download('punkt')
 def main():
     # read the dataset
     dataset = pd.read_csv("dataset/sentiment1/Emotion_final.csv")
-    labels = dataset["Emotion"].unique() # sadness, anger, love, surprise, fear, happy
+    # sadness, anger, love, surprise, fear, happy
+    labels = dataset["Emotion"].unique()
     dataset = dataset.dropna()
 
     # store input data and labels
-    in_data = dataset.drop('Emotion', axis=1)
+    text_data = dataset.drop('Emotion', axis=1)
     label_data = dataset["Emotion"]
 
     # tokenize strings
     new_data = []
-    for s in in_data["Text"]:
+    for s in text_data["Text"]:
         tok = word_tokenize(s.lower())
         new_data.append(tok)
 
@@ -40,7 +42,22 @@ def main():
         # set the data without stopwords
         new_data[i] = tmp
 
-    print(new_data[0])
+    # print(new_data[0])
+
+    # condense all words to their stems
+    ps = PorterStemmer()
+    for i, n in enumerate(new_data):
+        tmp = []
+        for str in n:
+            tmp.append(ps.stem(str))
+        # set the data as the stems
+        new_data[i] = tmp
+
+    # print for debugging
+    """
+    for i in range(5):
+        print(new_data[i])
+    """
 
 
 if __name__ == "__main__":
