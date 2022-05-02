70d860aa2fe25a4bc758fe55f63dba0847b13eb4
JustSomeCarbon <noe.garcia@student.nmt.edu>
2022-05-01 21:22:49 -0600

    feat: constructing Naive Bayes function

--- naive_bayes.py
+++ naive_bayes.py
@@ -4,6 +4,8 @@
 #       implementation. The program expects the input to be already cleaned.
 #
 
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB # multinomial naive bayes
 import pandas as pd
 import numpy as np
 from bow import *
@@ -18,9 +20,24 @@ def main():
 
     # call for bag of words over data
     bag_data = Bow(text_data, label_data)
-    print(bag_data.bag)
+    #print(bag_data.bag)
 
-    return
+    # create the Naive Bayes model
+    nb_model(label_data, bag_data)
+
+
+#
+# nb_model - takes the label data from the dataset and a bag-of-words object
+#       to create a multinomial Naive Bayes model
+#
+def nb_model(label_data, bag_data):
+    # split the training and testing dataset
+    x_train, x_test, y_train, y_test = train_test_split(bag_data.bag, label_data, test_size=0.2)
+    
+    # create the model
+    mnd = MultinomialNB()
+    mnd.fit(x_train, y_train)
+
 
 
 if __name__ == '__main__':
