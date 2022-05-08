c82f07e463ae44d8d188cbdd29c5cfad57651bba
JustSomeCarbon <noe.garcia@student.nmt.edu>
2022-05-08 12:36:25 -0600

    feat: added nb over tfidf

--- naive_bayes.py
+++ naive_bayes.py
@@ -8,18 +8,14 @@ import string
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB # multinomial naive bayes
 from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import argparse
 import pandas as pd
 import numpy as np
 from bow import *
 
-# global filenames
-nb_bow_filename = "nb_bow.out"
-# global file variables
-nb_bow_file = open(nb_bow_filename, "w+")
-
 # increase epoch to find better mean accuracy
-epoch = 20
+epoch = 5
 run_val = 0
 
 # argparse definitions
@@ -30,6 +26,19 @@ args = vars(ap.parse_args())
 pre = args['pre_train']
 
 
+# global filenames
+nb_bow_filename = "nb_bow.out"
+nb_tf_filename = "nb_tfidf.out"
+# global file variables
+if pre == 'bow':
+    nb_bow_file = open(nb_bow_filename, "w+")
+else:
+    nb_tfidf_file = open(nb_tf_filename, "w+")
+
+
+#
+# MAIN FUNCTION
+#
 def main():
     print("Running Naive Bayes Sentiment Analysis Model Construction")
 
@@ -68,13 +77,22 @@ def nb_bow_func(text_data, label_data):
 
 
 #
-# nb_tfidf - 
+# nb_tfidf - overhead function that transforms the given text data into tf-idf
+#       vector representations. Those vectors are then utilized in the construction
+#       and evaluation of a Naive Bayes movel over the data.
 #
 def nb_tfidf_func(text_data, label_data):
-    return
+    print("Transforming dataset with TF-IDF...")
+
+    split_values = [0.2, 0.25, 0.3, 0.35, 0.4]
+    print("Constructing Naive Bayes model...")
+    for split_value in split_values:
+        for i in range(epoch):
+            nb_model_tfidf(label_data, text_data, split_value)
+
 
 #
-# nb_model - takes the label data from the dataset and a bag-of-words object
+# nb_model_bow - takes the label data from the dataset and a bag-of-words object
 #       to create a multinomial Naive Bayes model
 #
 def nb_model_bow(label_data, bag_data, splt_val):
@@ -94,10 +112,44 @@ def nb_model_bow(label_data, bag_data, splt_val):
     write_out(nb_bow_filename, acc, splt_val)
 
 
+#
+# nb_model_tfidf - takes the label data from the dataset and the text data to
+#       perform TF-IDF over the dataset and creates a multinomial Naive Bayes model
+#
+def nb_model_tfidf(label_data, text_data, splt_val):
+    x_train, x_test, y_train, y_test = train_test_split(text_data, label_data, test_size=splt_val)
+    
+    # create the term frequency vectorizer object
+    #tf_vect = CountVectorizer()
+    tf_vect = TfidfVectorizer()
+    # trained vectorizer
+    tf_train = tf_vect.fit_transform(x_train)
+    tf_test = tf_vect.transform(x_test)
+    #print("train shape: {}".format(tf_train.shape))
+    #print("test shape: {}".format(tf_test.shape))
+
+    # create the Naive Bayes model
+    mnd = MultinomialNB()
+    mnd.fit(tf_train, y_train)
+
+    # determine the accuracy of the model
+    naive_predict = mnd.predict(tf_test)
+    acc = accuracy_score(y_test, naive_predict)
+    print(" Model evaluation run test split:", splt_val)
+    print("  Accuracy of Naive Bayes using tf-idf:", (acc*100))
+    write_out(nb_tf_filename, acc, splt_val)
+
+
+#
+# write_out - write the accuracy out the the appropriate file
+#
 def write_out(filename, acc_val, splt_val):
     if (filename == nb_bow_filename):
         acc = str(run_val) + "::" + str(splt_val) + ": Accuracy Val: " + str(acc_val*100) + "\n"
         nb_bow_file.write(acc)
+    elif (filename == nb_tf_filename):
+        acc = str(run_val) + "::" + str(splt_val) + ": Accuracy Val: " + str(acc_val*100) + "\n"
+        nb_tfidf_file.write(acc)
 
 
 if __name__ == '__main__':
