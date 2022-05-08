e00df3b82f9358c505d73cf194641b260852e5f2
JustSomeCarbon <noe.garcia@student.nmt.edu>
2022-05-07 19:55:57 -0600

    feat: completed NB over bow

--- naive_bayes.py
+++ naive_bayes.py
@@ -4,13 +4,35 @@
 #       implementation. The program expects the input to be already cleaned.
 #
 
+import string
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB # multinomial naive bayes
+from sklearn.metrics import accuracy_score
+import argparse
 import pandas as pd
 import numpy as np
 from bow import *
 
+# global filenames
+nb_bow_filename = "nb_bow.out"
+# global file variables
+nb_bow_file = open(nb_bow_filename, "w+")
+
+# increase epoch to find better mean accuracy
+epoch = 20
+run_val = 0
+
+# argparse definitions
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--pre_train", default='bow', choices=['bow', 'tf-idf'])
+
+args = vars(ap.parse_args())
+pre = args['pre_train']
+
+
 def main():
+    print("Running Naive Bayes Sentiment Analysis Model Construction")
+
     dataset = pd.read_csv("cleandata.csv")
 
     # divide the dataset
@@ -18,26 +40,64 @@ def main():
     label_data = dataset["Emotion"]
     total_entries = len(text_data)
 
+    if pre == 'bow':
+        nb_bow_func(text_data, label_data)
+    else:
+        nb_tfidf_func(text_data, label_data)
+
+
+#
+# nb_bow_func - overhead function that creates a bag of words object from
+#       the text data. The bow is then utilized in the construction and
+#       evaluation of a Naive Bayes model over the data.
+#
+def nb_bow_func(text_data, label_data):
+    print("Generating bag-of-words from dataset...")
     # call for bag of words over data
     bag_data = Bow(text_data, label_data)
     #print(bag_data.bag)
+    print("bag-of-words object initialized!")
 
+    split_values = [0.2, 0.25, 0.3, 0.35, 0.4]
+
+    print("Constructing Naive Bayes model...")
     # create the Naive Bayes model
-    nb_model(label_data, bag_data)
+    for split_value in split_values:
+        for i in range(epoch):
+            nb_model_bow(label_data, bag_data, split_value)
 
+
+#
+# nb_tfidf - 
+#
+def nb_tfidf_func(text_data, label_data):
+    return
 
 #
 # nb_model - takes the label data from the dataset and a bag-of-words object
 #       to create a multinomial Naive Bayes model
 #
-def nb_model(label_data, bag_data):
+def nb_model_bow(label_data, bag_data, splt_val):
     # split the training and testing dataset
-    x_train, x_test, y_train, y_test = train_test_split(bag_data.bag, label_data, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(bag_data.bag, label_data, test_size=splt_val)
     
     # create the model
     mnd = MultinomialNB()
     mnd.fit(x_train, y_train)
 
+    # generate the accuracy score for the model
+    naive_predict = mnd.predict(x_test)
+
+    acc = accuracy_score(y_test, naive_predict)
+    print(" Model evaluation run test split:", splt_val)
+    print("  Accuracy of Naive Bayes using BOW:", (acc*100))
+    write_out(nb_bow_filename, acc, splt_val)
+
+
+def write_out(filename, acc_val, splt_val):
+    if (filename == nb_bow_filename):
+        acc = str(run_val) + "::" + str(splt_val) + ": Accuracy Val: " + str(acc_val*100) + "\n"
+        nb_bow_file.write(acc)
 
 
 if __name__ == '__main__':
